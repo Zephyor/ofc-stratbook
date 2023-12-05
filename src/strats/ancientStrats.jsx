73bd95860fbd012f403f4be628be635ee6cbce7e
Zephyor <87764327+Zephyor@users.noreply.github.com>
2023-12-05 23:50:07 +0100

    feat: GR ancient

--- src/strats/ancientStrats.jsx
+++ src/strats/ancientStrats.jsx
@@ -1,7 +1,60 @@
 const ancientStrategies = [
-    { title: 'Ancient', content: 'Content for Strategy 1.' },
-    { title: 'Strategy 2', content: 'Content for Strategy 2.' },
-    // Add more strategies as needed
-  ];
-  
-  export default ancientStrategies;
+  {
+    title: 'CT Pistol Double Dual Cave',
+    content: (
+      <ul>
+        <li style={{ marginBottom: '0.6rem' }}>
+          <strong>Rôle 1</strong> (Pilou) - Achète Kevlar, Joue Long B
+        </li>
+        <li style={{ marginBottom: '0.6rem' }}>
+          <strong>Rôle 2</strong> (Zephyor) - Achète 2x Dual / Flash, Drop les
+          Dual à NAT' et Sakeazey et joue l'info mid en shoulderpeek
+        </li>
+        <li style={{ marginBottom: '0.6rem' }}>
+          <strong>Rôle 3</strong> (NAT') - Achète Kevlar, Joue close cave en
+          cross avec Sakeazey au Dual
+        </li>
+        <li style={{ marginBottom: '0.6rem' }}>
+          <strong>Rôle 4</strong> (Sakeazey) - Achète Kevlar, Joue close cave en
+          cross avec NAT' au Dual Dual
+        </li>
+        <li style={{ marginBottom: '0.6rem' }}>
+          <strong>Rôle 5</strong> (Nino) - Achète Kit / Smoke, Joue retake A
+        </li>
+      </ul>
+    ),
+    side: 'CT',
+  },
+  {
+    title: 'T Pistol Fast Exec B',
+    content: (
+      <ul>
+        <li style={{ marginBottom: '0.6rem' }}>
+          <strong>Rôle 1</strong> (Pilou) - Achète Kevlar, se rapproche de la
+          cave et joue en 2ème temps
+        </li>
+        <li style={{ marginBottom: '0.6rem' }}>
+          <strong>Rôle 2</strong> (Zephyor) - Achète Smoke / 2x Flash, Lance
+          tout les stuff exec et garde le back rampe
+        </li>
+        <li style={{ marginBottom: '0.6rem' }}>
+          <strong>Rôle 3</strong> (NAT') - Achète Smoke / Molo, Drop les stuff
+          dans le coin pour exec et se prépare à explo rampe et plante la bombe
+        </li>
+        <li style={{ marginBottom: '0.6rem' }}>
+          <strong>Rôle 4</strong> (Sakeazey) - Achète Kevlar, se prépare à explo
+          BP et à jouer la cave en même temps que Pilou cross avec NAT' au Dual
+          Dual
+        </li>
+        <li style={{ marginBottom: '0.6rem' }}>
+          <strong>Rôle 5</strong> (Nino) - Achète Kit / Smoke, se prépare à
+          explo BP et à jouer la cave en même temps que Pilou
+        </li>
+      </ul>
+    ),
+    side: 'T',
+  },
+  // Add more strategies as needed
+];
+
+export default ancientStrategies;
