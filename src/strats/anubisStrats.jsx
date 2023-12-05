c569ff8a153340a345b6da26777e1cbd6005a580
Zephyor <87764327+Zephyor@users.noreply.github.com>
2023-12-05 23:42:02 +0100

    feat: GR CT Anubis

--- src/strats/anubisStrats.jsx
+++ src/strats/anubisStrats.jsx
@@ -1,7 +1,34 @@
 const anubisStrategies = [
-    { title: 'Anubis', content: 'Content for Strategy 1.' },
-    { title: 'Strategy 2', content: 'Content for Strategy 2.' },
-    // Add more strategies as needed
-  ];
-  
-  export default anubisStrategies;
+  {
+    title: 'CT Pistol Triple Dual',
+    content: (
+      <ul>
+        <li style={{ marginBottom: '0.6rem' }}>
+          <strong>Rôle 1</strong> (Pilou) - Achète Dual x2 / Flash, Drop les
+          dual à Nino et Sakeazey et flash le main B à 1:40 si les terro n'ont
+          pas rush
+        </li>
+        <li style={{ marginBottom: '0.6rem' }}>
+          <strong>Rôle 2</strong> (Zephyor) - Achète Dual / Smoke / Flash, Drop
+          les Dual et la smoke à NAT' et joue l'info mid en coup d'épaules
+        </li>
+        <li style={{ marginBottom: '0.6rem' }}>
+          <strong>Rôle 3</strong> (NAT') - Achète Kevlar, Joue contact A au Dual
+        </li>
+        <li style={{ marginBottom: '0.6rem' }}>
+          <strong>Rôle 4</strong> (Nino) - Achète Kevlar, Joue contact cave au
+          Dual
+        </li>
+        <li style={{ marginBottom: '0.6rem' }}>
+          <strong>Rôle 5</strong> (Sakeazey) - Achète Kevlar, Joue close main B
+          au Dual
+        </li>
+      </ul>
+    ),
+    side: 'CT',
+  },
+  { title: 'Strategy 2', content: 'Content for Strategy 2.' },
+  // Add more strategies as needed
+];
+
+export default anubisStrategies;
