716a8090a354e62429661714d2ea9a198683d2fc
Zephyor <87764327+Zephyor@users.noreply.github.com>
2023-12-05 03:40:16 +0100

    fix: bloc separateur

--- src/scenes/MapStrats.jsx
+++ src/scenes/MapStrats.jsx
@@ -1,6 +1,5 @@
 import strategiesMap from '../strats/strategiesMap';
 import Strat from '../components/Strat';
-import { Fragment } from 'react';
 
 const MapStrats = ({ mapName }) => {
   // Get the strategy array based on the mapName
@@ -8,29 +7,53 @@ const MapStrats = ({ mapName }) => {
   console.log('mapName:', mapName);
   console.log('mapStrategies:', mapStrategies);
 
+  const tStrategies = mapStrategies.filter((strategy) => strategy.side === 'T');
+  const ctStrategies = mapStrategies.filter(
+    (strategy) => strategy.side === 'CT'
+  );
+
   return (
     <div style={{ margin: '1rem', color: '#fff' }}>
       <h2 style={{ color: '#FF7833', fontSize: '200%', marginLeft: '3rem' }}>
         {mapName} Strategies
       </h2>
-      {mapStrategies.map((strategy, index) => (
-        <Fragment key={strategy.title}>
-          {index > 0 && strategy.side !== mapStrategies[index - 1].side && (
-            // Ajouter un séparateur si le côté a changé
-            <div
-              style={{
-                borderBottom: '2px solid #fff',
-                margin: '1rem 0',
-                borderColor: '#FF7833',
-              }}
-            ></div>
-          )}
-          <Strat
-            title={strategy.title}
-            content={strategy.content}
-            side={strategy.side}
-          />
-        </Fragment>
+      <div
+        style={{
+          borderBottom: '2px solid #fff',
+          margin: '1rem 0',
+          borderColor: '#f1f1f1',
+        }}
+      ></div>
+
+      {/* Bloc Strat T */}
+      <h3 style={{ color: '#F5AF0D', marginTop: '1rem' }}>Terrorists (T)</h3>
+      {tStrategies.map((strategy) => (
+        <Strat
+          key={strategy.title}
+          title={strategy.title}
+          content={strategy.content}
+          side={strategy.side}
+        />
+      ))}
+      <div
+        style={{
+          borderBottom: '2px solid #fff',
+          margin: '1rem 0',
+          borderColor: '#f1f1f1',
+        }}
+      ></div>
+
+      {/* Bloc Strat CT */}
+      <h3 style={{ color: '#F5AF0D', marginTop: '1rem' }}>
+        Counter-Terrorists (CT)
+      </h3>
+      {ctStrategies.map((strategy) => (
+        <Strat
+          key={strategy.title}
+          title={strategy.title}
+          content={strategy.content}
+          side={strategy.side}
+        />
       ))}
     </div>
   );
